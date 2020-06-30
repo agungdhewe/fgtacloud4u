a178d96ead6b3ffff04c01fe1bd12a1c60896860
agungdhewe <agung_dhewe@yahoo.com>
2020-07-01 06:28:32 +0700

    update dev index

--- index.php
+++ index.php
@@ -1,11 +1,24 @@
 <html>
 <head>
-	<title>FGTA Cloud 4</title>
+	<title>FGTA Cloud 4 Development</title>
 </head>
 <body>
-	Redirecting...
-	<script>
-	location.href='public';
-	</script>
+	<h1>FGTA Development</h1>
+
+	<div>
+		<a href="public">Default</a>
+	</div>
+	<div>
+		<a href="public_fgta">FGTA</a>
+	</div>
+	<div>
+		<a href="public_pt">Ferrine</a>
+	</div>
+	<div>
+		<a href="public_tvone">TVOne</a>
+	</div>
+	<div>
+		<a href="public_ecomm">e-Commerce</a>
+	</div>
 </body>
 </html>
