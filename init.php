cae5f7896291e53e36189c3456eb3ce7efce5ea3
agungdhewe <agung_dhewe@yahoo.com>
2020-07-11 08:02:00 +0700

    add project initiator

---
+++ init.php
@@ -0,0 +1,70 @@
+<?php
+
+
+class init {
+
+	function __construct($args) {
+		$this->args = $args;
+	}
+
+
+	function CheckCommand() {
+		if (count($this->args) < 2) {
+			$msg  = "Target direktori tujuan belum ditentukan.\n";
+			$msg .= "syntax:  php init.php <nama_project_tanpa_spasi>";
+			throw new Exception($msg);
+		}
+
+		$this->projectname = $this->args[1];
+	}
+
+	function CreateProjectDir($namaproject) {
+		$path = dirname(__FILE__)."/public_$namaproject";
+		if (is_dir($path) || is_file($path)) {
+			$msg  = "\x1b[33m"."public_$namaproject"."\x1b[0m"." konflik dengan path "."\x1b[1m".$path."\x1b[0m"." yang sudah ada\n";
+			throw new Exception($msg);			
+		}
+
+		mkdir($path);
+		$this->projectpath = $path;
+	}
+
+	
+	function CreateSymlink() {
+
+	}
+
+
+	function CopyFromPublic() {
+		//$source = dirname(__FILE__)."/public/dbconfig";
+		$tocopy = ['dbconfig.php', 'favicon.ico', 'manifest.json'];
+		foreach ($tocopy as $filename) {
+			$source = dirname(__FILE__)."/public/$filename";
+			$target = $this->projectpath."/$filename";
+
+			echo "copying $filename ...\r\n";
+			copy($source, $target);
+		}
+
+	}
+
+
+	function execute() {
+		try {
+			$this->CheckCommand();
+			$this->CreateProjectDir($this->projectname);
+			$this->CreateSymlink();
+			$this->CopyFromPublic();
+			
+
+		} catch (Exception $ex) {
+			echo "\x1b[31m"."ERROR"."\x1b[0m"."\r\n";
+			echo $ex->getMessage();
+		} finally {
+			die("\r\n\r\n");
+		}
+	}
+
+}
+
+(new init($argv))->execute();
